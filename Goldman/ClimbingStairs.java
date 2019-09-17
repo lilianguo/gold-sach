7b883eadeac224ce467d499dff3fa8203a44102b
Rebecca Guo <rebeccaguospecial@gmail.com>
2019-09-16 22:46:24 -0700

    Create ClimbingStairs.java

---
+++ Goldman/ClimbingStairs.java
@@ -0,0 +1,16 @@
+// 70	Climbing Stairs    
+class ClimbingStairs {
+    public int climbStairs(int n) {
+        if (n <= 2) {
+            return n;
+        }
+        int[] dp = new int[n + 1];
+        dp[0] = 0;
+        dp[1] = 1;
+        dp[2] = 2;
+        for (int i = 3; i <= n; i++) {
+            dp[i] = dp[i - 1] + dp[i - 2];ß
+        }
+        return dp[n];
+    }
+}
