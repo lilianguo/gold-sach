c59cc4b9496c17ad26e231ef52e2f5000f073265
Rebecca Guo <rebeccaguospecial@gmail.com>
2019-09-18 07:34:08 -0700

    Create ReverseString.java

---
+++ ReverseString.java
@@ -0,0 +1,17 @@
+class ReverseString {
+    public void reverseString(char[] s) {
+        if (s == null || s.length == 0) {
+            return;
+        }
+        int left = 0;
+        int right = s.length - 1;
+        while(left < right) {
+            char tmp = s[left];
+            s[left] = s[right];
+            s[right] = tmp;
+            left++;
+            right--;
+        }
+        return;
+    }
+}
